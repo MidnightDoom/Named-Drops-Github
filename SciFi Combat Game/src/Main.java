4917ab968b1e8d36de0c7ba2e1e308a61ccb3196
MidnightDoom <1108608@apps.nsd.org>
2025-04-18 07:23:05 -0700

    Main fix

--- SciFi Combat Game/src/Main.java
+++ SciFi Combat Game/src/Main.java
@@ -1,55 +1,69 @@
-import javax.swing.JFrame;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
 public class Main extends JFrame {
 
-    public static World world = new World();
+    private boolean running = false;
+
+    public static final int WIDTH = 400;
+    public static final int HEIGHT = 400;
+
+    // Milliseconds per step
+    public static int timeStep = 10;
+    // Multiplies all motion and force values (slows sim even at low time steps)
+    public static double timeMultiplier = 0.05;
+    // Multiplies gravity calculations (think of it as this world's gravitational constant)
+    public static double gravityMultiplier = 100;
+    // Modifies distances (large distances can be treated as smaller)
+    public static double distanceMultiplier = 0.001;
+
+    public static World world;
 
     public static void main(String[] args) throws InterruptedException {
-		Main theGUI = new Main();
-		SwingUtilities.invokeLater(() -> theGUI.createFrame(theGUI));
-		synchronized (theGUI ) {
-			theGUI.wait();
-		}
+
+        Main theGUI = new Main();
+        SwingUtilities.invokeLater(() -> theGUI.createFrame(theGUI));
+        synchronized (theGUI) {
+            theGUI.wait();
+        }
 
         world = Worlds.lunaBurns;
-		
-		while (true) {
-			System.out.println("Start Sim");
-			theGUI.startSim();
-			System.out.println("Sim stopped");
-		}
-	}
+        //world = Worlds.randomWorld();
+
+        while (true) {
+            System.out.println("Sim started");
+            theGUI.startSim(timeStep);
+            System.out.println("Sim stopped");
+        }
+    }
 
     public void createFrame(Object semaphore) {
-        
-		this.setTitle("My animation");
-		this.setSize(400,400);
+        this.setTitle("Space Sim");
+        this.setSize(WIDTH, HEIGHT);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    
-		MainPanel panel = new MainPanel();
-		
-		panel.setBounds(0, 0, 400, 400);
-		this.add(panel);
-		panel.setVisible(true);
-		this.setVisible(true);
+        MainPanel mainPanel = new MainPanel();
+        mainPanel.setVisible(true);
+        mainPanel.setSize(WIDTH, HEIGHT);
+        mainPanel.setBounds(0, 0, WIDTH, HEIGHT);
+        this.add(mainPanel);
 
-		synchronized (semaphore) {
-			semaphore.notify();
-		}
-	}
+        this.setVisible(true);
+        synchronized (semaphore) {
+            semaphore.notify();
+        }
+    }
 
-    public void startSim() {
-		boolean running = true;
-		try {			
-			while (running) {
+    public void startSim(int delay) {
+        running = true;
+
+        try {
+            while (running) {
                 world.update();
-				repaint();
-				Thread.sleep(1000);
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+                repaint();
+                Thread.sleep(delay);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
